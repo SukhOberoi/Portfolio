bec561fb55b660885e92a6f18b45584b5f1a5556
Sukh Oberoi <sukhoberoi24@gmail.com>
2024-08-06 11:15:19 +0530

    Update Skills.jsx

--- src/routes/Skills.jsx
+++ src/routes/Skills.jsx
@@ -76,7 +76,7 @@ export default function Skills() {
 									BTech - Computer Science
 								</p>
 								2022-2026
-								<br /> SRM University <br /> CGPA: 8.91
+								<br /> SRM University <br /> CGPA: 9.05
 							</li>
 							<li>
 								<p className="text-lg">Schooling (CBSE)</p>
