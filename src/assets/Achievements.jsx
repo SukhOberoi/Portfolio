7626fb8fb1d8c41afe295f89c7d53dc18a478469
Sukh Oberoi <sukhoberoi24@gmail.com>
2024-09-11 01:58:06 +0530

    added achievement

--- src/assets/Achievements.jsx
+++ src/assets/Achievements.jsx
@@ -1,5 +1,16 @@
 const Achievements = [
 	{
+		Achievement: "Winner (₹1500 Prize) - Innovate AI Ideathon",
+		Description:
+			"",
+		IssuedBy: "IE(I) KTR w/ Department of Computing Technologies SRMIST",
+		Date: "Aug 2024",
+		Images: [
+			"./achieves/ideathonPrizeDist.jpeg",
+			"./achieves/ideathonCert.jpeg",
+		],
+	},
+	{
 		Achievement: "Google India's Code to Learn - Runner Up",
 		Description:
 			"Proud Runner-up in Google India's Code to Learn competition in 2017, where I demonstrated my skills by creating the innovative 'School Assistant' Android app using MIT's App Inventor. This experience fueled my desire to solve real-world problems through technology.",
